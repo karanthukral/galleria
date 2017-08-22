862fb0817bab3e05f77788e92820380de9f86c90
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-08-22 09:57:11 -0400

    some speed sttuf

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+---
+# Don't delete this line. This main JS file needs this front matter
+---
+
+{% include_relative photo_swipe.js %}
+{% include_relative fontawesome.min.js %}
+
+# // NOTE brands is weird 
